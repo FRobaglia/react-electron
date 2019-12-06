91d5cb3ec2dec0f8e91fa483580a33d9391842c7
FRobaglia <felix.robaglia@gmail.com>
2019-12-06 02:54:20 +0100

    add base scss

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,14 +1,10 @@
 import React from 'react'
 
 class HelloWorld extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  
   render() {
     return (
       <h1>
-        Hello World
+        Hello {this.props.world}
       </h1>
     )
   }
